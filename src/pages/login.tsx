493a4dbc91cbb597c139ce501fb2d9acb1b2f9f3
shin-tc-10 <shin.nkt.09@gmail.com>
2023-09-02 15:18:34 +0900

    login 機能実装

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,34 @@ import Head from "next/head";
 import { useAuth } from "@/context/auth";
 
 const Login = () => {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const router = useRouter();
+
+    const { login } = useAuth();
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        try {
+            const response = await apiClient.post("/auth/login", {
+                email,
+                password,
+            });
+
+            const token = response.data.token;
+
+            login(token);
+
+            router.push("/");
+
+        } catch (err) {
+            alert("入力内容が正しくありません。");
+        }
+    };
+
+
   return (
     <div>
       <Head>
@@ -13,14 +41,28 @@ const Login = () => {
       </div>
       <div>
         <div>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div>
               <label>メールアドレス</label>
-              <input type="email" />
+              <input 
+                id="email"
+                name="email"
+                type="email" 
+                autoComplete="email"
+                required
+                onChange={(e: React.ChangeEvent<HTMLInputElement> => setEmail(e.target.value))}
+                />
             </div>
             <div>
               <label>パスワード</label>
-              <input type="password" />
+              <input 
+                id="password"
+                name="password"
+                type="password"
+                autoComplete="current-password"
+                required
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+                />
             </div>
             <div>
               <button type="submit">ログイン</button>
